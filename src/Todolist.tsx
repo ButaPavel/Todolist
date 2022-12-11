8aa381fce2ca45458640b7304e347ec9bd8b8326
ButaPavel <buta.pavel84@gmail.com>
2022-12-11 22:38:20 +0300

    01 Homework +

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -7,16 +7,41 @@ type TasksType = {
     isDone: boolean
 }
 
-type PropsType = {
+type DataType = {
     title: string
     tasks: Array<TasksType>
     students: Array<string>
 }
-export function  Tasks(props: PropsType) {
+
+type PropsType = {
+    data: DataType
+}
+
+export function Tasks(props: PropsType) {
     return (
 
         <div>
+
             <h3>{props.data.title}</h3>
+
+            <ul>
+                {props.data.tasks.map(t => {
+                    return (
+                        <li>
+                            <span>{t.taskId}</span>
+                            <span>{t.title}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <ul>
+                {props.data.students.map(t => {
+                    return (
+                        <li>{t}</li>
+                    )
+                })}
+            </ul>
 
         </div>
 
